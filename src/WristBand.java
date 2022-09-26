900dce90efa64fd539ada4e3aeadf59a77e37576
Joel7721 <mensahjl@mail.uc.edu>
2022-09-25 22:30:38 -0400

    Completed wristband program.

---
+++ src/WristBand.java
@@ -0,0 +1,11 @@
+public class WristBand {
+    public static void main(String[] args) {
+        int userAge = 31;
+
+
+        if(userAge >= 21){
+            System.out.println("Your age is " + userAge);
+            System.out.println("You get a wrist band.");
+        }
+    }
+}
